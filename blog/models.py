82fb21efb8119de064d629a3cf1297669a4b94c9
Umorjyoti Chetia <umorjyotichetia@gmail.com>
2020-08-12 19:29:33 +0530

    resume and pic added

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     pub_date=models.DateTimeField(auto_now=True)
     blogpic = models.ImageField(upload_to="images/")
     body=models.TextField()
+
+    def summary(self):
+        return self.body[:100]
+
+
+    def __str__(self):
+        return self.title
+
+    def pubdate(self):
+        return self.pub_date.strftime('%b %e %Y')
--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Jobs(models.Model):
     image = models.ImageField(upload_to="images/")
     header =models.CharField(max_length=100)
-    
+
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.header
