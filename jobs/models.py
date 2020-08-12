f9165411ecf3cba162bae108af3aeac5f5cc4ebd
Umorjyoti Chetia <umorjyotichetia@gmail.com>
2020-08-12 11:34:32 +0530

    Bootstrap 4 home page

--- jobs/models.py
+++ jobs/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Jobs(models.Model):
     image = models.ImageField(upload_to="images/")
     header =models.CharField(max_length=100)
+    
     summary = models.CharField(max_length=200)
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Jobs
+def home(request):
+    jobs=Jobs.objects
+    return render(request,'jobs/home.html',{'jobs':jobs})
